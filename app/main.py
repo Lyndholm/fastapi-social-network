977ee815971ca90efe51c29d6a0dcdfff1631943
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2021-11-07 17:09:35 +0300

    Schemas and response models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
+from typing import List
+
 from fastapi import Depends, FastAPI, HTTPException, Response, status
-from pydantic import BaseModel
 from sqlalchemy.orm.session import Session
 
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -10,38 +11,32 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello world!"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
 
-@app.get("/posts/{post_id}")
+@app.get("/posts/{post_id}", response_model=schemas.Post)
 async def get_post(post_id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} was not found")
-    return {"data": post}
+    return post
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_posts(payload: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+async def create_posts(payload: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**payload.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -54,11 +49,11 @@ async def delete_post(post_id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{post_id}")
-async def update_post(post_id: int, payload: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{post_id}", response_model=schemas.Post)
+async def update_post(post_id: int, payload: schemas.PostCreate, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == post_id)
     if post.first() is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} was not found")
     post.update(payload.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post.first()}
+    return post.first()
