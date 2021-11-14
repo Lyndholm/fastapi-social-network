b141335b6c9d2e586511536180c62bcf2d367a00
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2021-11-14 21:58:10 +0300

    Post owner logic

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.schema import Column, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP, Boolean, Integer, String
 
 from .database import Base
@@ -13,6 +13,7 @@ class Post(Base):
     content = Column(String, nullable=False)
     published = Column(Boolean, nullable=False, server_default="True")
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("NOW()"))
+    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
 
 class User(Base):
--- app/routers/post.py
+++ app/routers/post.py
@@ -29,7 +29,7 @@ async def create_posts(
     db: Session = Depends(get_db),
     current_user: int = Depends(oauth2.get_current_user)
 ):
-    new_post = models.Post(**payload.dict())
+    new_post = models.Post(owner_id=current_user.id, **payload.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -37,20 +37,40 @@ async def create_posts(
 
 
 @router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(post_id: int, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id)
-    if post.first() is None:
+async def delete_post(
+    post_id: int,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+):
+    post_query = db.query(models.Post).filter(models.Post.id == post_id)
+    post = post_query.first()
+
+    if post is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} was not found")
-    post.delete(synchronize_session=False)
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="not authorized to perform requested action")
+
+    post_query.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.put("/{post_id}", response_model=schemas.Post)
-async def update_post(post_id: int, payload: schemas.PostCreate, db: Session = Depends(get_db)):
-    post = db.query(models.Post).filter(models.Post.id == post_id)
-    if post.first() is None:
+async def update_post(
+    post_id: int, payload: schemas.PostCreate,
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user)
+):
+    post_query = db.query(models.Post).filter(models.Post.id == post_id)
+    post = post_query.first()
+
+    if post is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f"post with id {post_id} was not found")
-    post.update(payload.dict(), synchronize_session=False)
+
+    if post.owner_id != current_user.id:
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="not authorized to perform requested action")
+
+    post_query.update(payload.dict(), synchronize_session=False)
     db.commit()
     return post.first()
