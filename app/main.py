c784457a9174e3e3f17cf9bc8d6813fbd47052f5
Lyndholm <52299118+Lyndholm@users.noreply.github.com>
2021-11-14 23:01:54 +0300

    :)

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from .routers import auth, post, user
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
